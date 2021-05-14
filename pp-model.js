912ebb1a8fec56c32c620bb97de335724751f7b1
carlos-sweb <c4rl0sill3sc4@gmail.com>
2021-05-14 12:47:04 -0400

    update

--- pp-model.js
+++ pp-model.js
@@ -5,24 +5,16 @@
  * Released under the MIT License
  */
 (function(global , factory ){
-
   var root = typeof self == 'object' && self.self === self && self ||
   typeof global == 'object' && global.global === global && global;
-
   if (typeof define === 'function' && define.amd) {
-
     define(['ppEvents','exports'], function(ppEvents, exports) {
-
       root.ppModel = factory(root, exports,ppEvents);
-
     });
-
   } else if (typeof exports !== 'undefined') {
-
     var ppEvents = {};
     try { ppEvents = require('ppEvents'); } catch (e) {}
     factory(root, exports, ppEvents );
-
   } else {
     root.ppModel = factory(root, {}, root.ppEvents );
   }
@@ -30,26 +22,14 @@
 })( this,(function( root , example , ppEvents ) {
   var data = {},
   toString = Object.prototype.toString,
-  isObject = function( value ){
-    return toString.call(value) === '[object Object]'
-  },
-  isNull = function( value){
-    return toString.call(value) === '[object Null]'
-  },
-  isString = function( value ){
-    return toString.call(value) === '[object String]'
-  },
-  isUndefined = function( value ){
-    return toString.call( value ) === '[object Undefined]'
-  },
+  isObject = function( value ){return toString.call(value) === '[object Object]'},
+  isNull = function( value){return toString.call(value) === '[object Null]'},
+  isString = function( value ){return toString.call(value) === '[object String]'},
+  isUndefined = function( value ){return toString.call( value ) === '[object Undefined]'},
   Events = isUndefined( ppEvents ) ? null :  new ppEvents(),
   ppModel = function( defaults, __model ){
-    if( isObject(defaults) ){
-      Object.assign(data,{...defaults})
-    }
-    if( isObject(__model) ){
-        Object.assign(data,{...__model})
-    }
+    if( isObject(defaults) ){ Object.assign(data,{...defaults}) }
+    if( isObject(__model) ){ Object.assign(data,{...__model}) }
   },
   proto = ppModel.prototype;
   proto.on = isNull(Events) ? function(){/*Include messague*/} : Events.on;
@@ -75,28 +55,13 @@
       }
     }
   }
-  proto.stringify = function( replacer , space ){
-    return JSON.stringify(data, replacer , space)
-  }
-
-  proto.keys = function(){
-    return Object.keys( {...data} )
-  }
-  proto.has = function(key){
-    return isString(key) ? data.hasOwnProperty(key) : false ;
-  }
-  proto.values = function(){
-    return Object.values( {...data} )
-  }
-  proto.getAll = function(){
-    return Object.assign({},{...data})
-  }
-  proto.isString = function( key ){
-    return this.has(key) ? toString.call( key ) === '[object String]' :  false ;
-  }
-  proto.isBoolean = function(){
-    return this.has(key) ? toString.call( key ) === '[object Boolean]' :  false ;
-  }
+  proto.stringify = function( replacer , space ){return JSON.stringify(data, replacer , space)}
+  proto.keys = function(){return Object.keys( {...data} )}
+  proto.has = function(key){return isString(key) ? data.hasOwnProperty(key) : false ;}
+  proto.values = function(){return Object.values( {...data} )}
+  proto.getAll = function(){return Object.assign({},{...data})}
+  proto.isString = function( key ){return this.has(key) ? toString.call( key ) === '[object String]' :  false ;}
+  proto.isBoolean = function(){return this.has(key) ? toString.call( key ) === '[object Boolean]' :  false ;}
   proto.isEmpty = function(){
     var value = data[key] || "";
       if( value == null ){
@@ -150,9 +115,7 @@
           return result;
         };
   }
-  var prepareModel = function( initializeData ){
-    return this.main( initializeData || {} );
-  }
+  var prepareModel = function( initializeData ){return this.main( initializeData || {} );}
   prepareModel.prototype.main = function( preOptions ){
     var defaults = null;
     if( isObject(preOptions) ){
